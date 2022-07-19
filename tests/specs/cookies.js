1d6958204756530e7f981ac5416950876eddb23b
Oluwaseun Otubanjo <omo1933@gmail.com>
2022-07-19 15:10:39 -0500

    formatted the tests

--- tests/specs/cookies.js
+++ tests/specs/cookies.js
@@ -1,22 +1,22 @@
-describe('cookie verification', function() {
-    it("Verify that after accepting the cookie notice, the user doesn't see the notice anymore.", function(browser) {
-      browser
-        .navigateTo('https://www.designory.com')
-        .waitForElementVisible('#CybotCookiebotDialog')
-        .click('#CybotCookiebotDialogBodyButtonAccept')
-        .assert.not.visible('#CybotCookiebotDialog')
-    }); 
+describe('cookie verification', function () {
+    it("Verify that after accepting the cookie notice, the user doesn't see the notice anymore.", function (browser) {
+        browser
+            .navigateTo('https://www.designory.com')
+            .waitForElementVisible('#CybotCookiebotDialog')
+            .click('#CybotCookiebotDialogBodyButtonAccept')
+            .assert.not.visible('#CybotCookiebotDialog')
+    });
 
-    it("Verify that after clearing cookies the “cookie notice” shows up again.", function(browser) {
+    it("Verify that after clearing cookies the “cookie notice” shows up again.", function (browser) {
         browser
-          .navigateTo('https://www.designory.com')
-          .waitForElementNotPresent('#CybotCookiebotDialog')
-          .click('.CookiebotWidget-logo')
-          .waitForElementVisible('#CookiebotWidget-buttons')
-          .click('#CookiebotWidget-btn-withdraw')
-          .click('#CookiebotWidget-btn-change')
+            .navigateTo('https://www.designory.com')
+            .waitForElementNotPresent('#CybotCookiebotDialog')
+            .click('.CookiebotWidget-logo')
+            .waitForElementVisible('#CookiebotWidget-buttons')
+            .click('#CookiebotWidget-btn-withdraw')
+            .click('#CookiebotWidget-btn-change')
         browser
-          .navigateTo('https://www.designory.com')
-          .waitForElementVisible('#CybotCookiebotDialog')
-      }); 
-  });
+            .navigateTo('https://www.designory.com')
+            .waitForElementVisible('#CybotCookiebotDialog')
+    });
+});
--- tests/specs/location.js
+++ tests/specs/location.js
@@ -1,11 +1,15 @@
-describe('location verification', function() {
-    it('Verify that the Chicago location is present in the footer on https://www.designory.com/locations/chicago', function(browser) {
-      browser
-        .navigateTo('https://www.designory.com/locations/chicago')
+describe('location verification', function () {
+    it('Verify that the Chicago location is present in the footer on https://www.designory.com/locations/chicago', function (browser) {
+        browser
+            .navigateTo('https://www.designory.com/locations/chicago')
+        browser.expect.element(".heading-7").text.to.equal('CHICAGO IL,')
         browser.expect.element('h1').text.to.equal('CHI')
         browser.expect.element('.grid-12:nth-child(2) > .side-section > p').text.to.equal("Phone: +1 312 729 4500")
         browser.expect.element('.location').to.have.attribute('href').which.equals("http://maps.google.com/?q= 225 N Michigan Ave, Suite 2100 Chicago, IL 60601")
+        //browser.expect.element('h2').to.have.css('font-size').which.equals('16px')
+        //The UI shows that the font size is 16px
         browser.expect.element('h2').to.have.css('font-size').which.equals('40px')
+        //There is no Address in the footer
         browser.expect.element('footer').text.to.contain('Chicago')
-    }); 
-  });
+    });
+});
