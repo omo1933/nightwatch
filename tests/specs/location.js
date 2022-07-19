d591f08cf02d42d0fde15a2b7f01c7d50af0bb92
Oluwaseun Otubanjo <omo1933@gmail.com>
2022-07-19 14:21:56 -0500

    added test for location

---
+++ tests/specs/location.js
@@ -0,0 +1,11 @@
+describe('location verification', function() {
+    it('Verify that the Chicago location is present in the footer on https://www.designory.com/locations/chicago', function(browser) {
+      browser
+        .navigateTo('https://www.designory.com/locations/chicago')
+        browser.expect.element('h1').text.to.equal('CHI')
+        browser.expect.element('.grid-12:nth-child(2) > .side-section > p').text.to.equal("Phone: +1 312 729 4500")
+        browser.expect.element('.location').to.have.attribute('href').which.equals("http://maps.google.com/?q= 225 N Michigan Ave, Suite 2100 Chicago, IL 60601")
+        browser.expect.element('h2').to.have.css('font-size').which.equals('40px')
+        browser.expect.element('footer').text.to.contain('Chicago')
+    }); 
+  });
