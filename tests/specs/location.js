be8463542f833a48dba7798b0115205a90fd9b64
Oluwaseun Otubanjo <omo1933@gmail.com>
2022-07-19 16:48:37 -0500

    updated location test

--- tests/specs/location.js
+++ tests/specs/location.js
@@ -6,10 +6,9 @@ describe('location verification', function () {
         browser.expect.element('h1').text.to.equal('CHI')
         browser.expect.element('.grid-12:nth-child(2) > .side-section > p').text.to.equal("Phone: +1 312 729 4500")
         browser.expect.element('.location').to.have.attribute('href').which.equals("http://maps.google.com/?q= 225 N Michigan Ave, Suite 2100 Chicago, IL 60601")
-        //browser.expect.element('h2').to.have.css('font-size').which.equals('16px')
-        //The UI shows that the font size is 16px
-        browser.expect.element('h2').to.have.css('font-size').which.equals('40px')
-        //There is no Address in the footer
-        browser.expect.element('footer').text.to.contain('Chicago')
+        //The UI shows that the font size is 16px instead of 40px
+        browser.expect.element('h2').to.have.css('font-size').which.equals('16px')
+        //There is no Address in the footer. Located under Hero image.
+        browser.expect.element('.grid-12:nth-child(1) > .side-section > p').text.to.contain('225 N Michigan Ave, Suite 2100')
     });
 });
